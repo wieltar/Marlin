7ad903a88a0ccc75ff6f591daca39c5f93cf1099
Wouter van Hees <whj.hees@student.avans.nl>
2020-01-30 15:13:46 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -721,7 +721,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98.9 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -935,7 +935,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -5, -1.51 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 20
